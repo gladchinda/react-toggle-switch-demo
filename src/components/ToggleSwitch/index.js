44958b69b3a4a3d74b95d8941100715b312aafbc
Glad Chinda <gladxeqs@gmail.com>
2018-08-23 17:08:26 +0100

    Fixed indentations

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -8,67 +8,67 @@ import './index.css';
 
 class ToggleSwitch extends Component {
 
-	state = {
-		enabled: this.enabledFromProps()
-	}
+  state = {
+    enabled: this.enabledFromProps()
+  }
 
-	isEnabled() {
-		return this.state.enabled;
-	}
+  isEnabled() {
+    return this.state.enabled;
+  }
 
-	enabledFromProps() {
-		let { enabled } = this.props;
-		enabled = isFunction(enabled) ? enabled() : enabled;
+  enabledFromProps() {
+    let { enabled } = this.props;
+    enabled = isFunction(enabled) ? enabled() : enabled;
 
-		return isBoolean(enabled) && enabled;
-	}
+    return isBoolean(enabled) && enabled;
+  }
 
-	toggleSwitch = evt => {
-		evt.persist();
-		evt.preventDefault();
+  toggleSwitch = evt => {
+    evt.persist();
+    evt.preventDefault();
 
-		const { onClick, onStateChanged } = this.props;
+    const { onClick, onStateChanged } = this.props;
 
-		this.setState({ enabled: !this.state.enabled }, () => {
-			const state = this.state;
-			const switchEvent = Object.assign(evt, { SWITCH_STATE: state });
+    this.setState({ enabled: !this.state.enabled }, () => {
+      const state = this.state;
+      const switchEvent = Object.assign(evt, { SWITCH_STATE: state });
 
-			isFunction(onClick) && onClick(switchEvent);
-			isFunction(onStateChanged) && onStateChanged(state);
-		});
-	}
+      isFunction(onClick) && onClick(switchEvent);
+      isFunction(onStateChanged) && onStateChanged(state);
+    });
+  }
 
-	render() {
-		const { enabled } = this.state;
-		const { enabled: _enabled, theme, onClick, className, onStateChanged, ...restProps } = this.props;
-		const switchTheme = (theme && isString(theme)) ? theme : 'default';
+  render() {
+    const { enabled } = this.state;
+    const { enabled: _enabled, theme, onClick, className, onStateChanged, ...restProps } = this.props;
+    const switchTheme = (theme && isString(theme)) ? theme : 'default';
 
-		const switchClasses = classnames(
-			`switch switch--${switchTheme}`,
-			className
-		)
+    const switchClasses = classnames(
+      `switch switch--${switchTheme}`,
+      className
+    )
 
-		const togglerClasses = classnames(
-			'switch-toggle',
-			`switch-toggle--${enabled ? 'on' : 'off'}`
-		)
+    const togglerClasses = classnames(
+      'switch-toggle',
+      `switch-toggle--${enabled ? 'on' : 'off'}`
+    )
 
-		return (
-			<div className={switchClasses} onClick={this.toggleSwitch} {...restProps}>
-				<div className={togglerClasses}></div>
-			</div>
-		)
-	}
+    return (
+      <div className={switchClasses} onClick={this.toggleSwitch} {...restProps}>
+        <div className={togglerClasses}></div>
+      </div>
+    )
+  }
 
 }
 
 ToggleSwitch.propTypes = {
-	theme: PropTypes.string,
-	enabled: PropTypes.oneOfType([
-		PropTypes.bool,
-		PropTypes.func
-	]),
-	onStateChanged: PropTypes.func
+  theme: PropTypes.string,
+  enabled: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.func
+  ]),
+  onStateChanged: PropTypes.func
 }
 
 export default ToggleSwitch;
